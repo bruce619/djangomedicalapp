2dbbba4bd5e2e85a02f304f9a2b7c8989f09c2e3
Chimuanya Ibecheozor <chimuanyaibecheozor@gmail.com>
2020-05-01 17:38:33 +0100

    Initial Commit

--- medicalapp/filters.py
+++ medicalapp/filters.py
@@ -6,8 +6,7 @@ class PatientFilter(django_filters.FilterSet):
 
     class Meta:
         model = MedicalHistory
-        fields = ('id', 'illness',)
+        fields = ('illness',)
         labels = {
             'illness': 'Choose any illness to filter by',
-            'id': 'Patient ID',
         }
